49492c3aba198b71ad99ab5266114eda2d2e7768
Justin Schaaf <justin@justinschaaf.com>
2024-05-26 04:03:28 -0700

    feat: add packaging and nixosModules to flake

--- flake.nix
+++ flake.nix
@@ -21,15 +21,92 @@
                     libuuid
                 ];
 
-                # Playwright is fucked
-                # https://nixos.wiki/wiki/Playwright
-                # https://www.giacomodebidda.com/posts/playwright-on-nixos/
                 # https://github.com/Automattic/node-canvas/issues/1947#issuecomment-991016228
                 LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath packages;
 
             };
 
+            # Adapted from https://blog.ielliott.io/nix-docs/mkDerivation.html#reference-inputs-src
+            # Full options at https://ryantm.github.io/nixpkgs/stdenv/stdenv/
+            packages.default = let
+                pkgJson = builtins.fromJSON (builtins.readFile ./package.json);
+            in pkgs.stdenv.mkDerivation rec {
+
+                name = pkgJson.name;
+                version = pkgJson.version;
+                src = ./.;
+
+                buildInputs = with pkgs; [ yarn-berry ];
+
+                buildPhase = ''
+                    yarn install
+                    yarn run build
+                '';
+
+                # https://stackoverflow.com/questions/46891622/run-yarn-in-a-different-path
+                installPhase = ''
+
+                    install -D -t $out/build/ $src/build/*
+                    install -D -t $out/ $src/.npmrc $src/.yarnrc.yml $src/package.json $src/yarn.lock
+
+                    yarn -cwd $out/ workspaces focus --production
+
+                '';
+
             };
+
+            nixosModules.default = { inputs, lib, config, pkgs, ... }: {
+
+                options.services.waffles.lol = {
+                    enable = lib.mkEnableOption "the waffles.lol website";
+                    hostName = lib.mkOption {
+                        type = lib.types.str;
+                        default = "localhost";
+                        description = "The domain at which the website should be accessed.";
+                    };
+                    port = lib.mkOption {
+                        type = lib.types.port;
+                        default = 3000;
+                        description = "The port the web server should be listening on.";
+                    };
+                    # description for bottom two are from https://kit.svelte.dev/docs/adapter-node#environment-variables
+                    shutdownTimeout = lib.mkOption {
+                        type = lib.types.int;
+                        default = 30;
+                        description = "The number of seconds to wait before forcefully closing any remaining connections after receiving a `SIGTERM` or `SIGINT` signal.";
+                    };
+                    idleTimeout = lib.mkOption {
+                        type = lib.types.nullOr lib.types.int;
+                        default = null;
+                        description = "The number of seconds after which the app is automatically put to sleep when receiving no requests. If not set, the app runs continuously.";
+                    };
+                };
+
+                config = lib.mkIf config.services.waffles.lol.enable {
+
+                    environment.systemPackages = with pkgs; [ nodejs-slim ];
+
+                    systemd.services."waffles.lol" = {
+                        script = "${pkgs.nodejs-slim}/bin/node ${self.outputs.packages.default}";
+                        wants = [ "network-online.target" ];
+                        after = [ "network-online.target" ];
+                        environment = {
+                            PORT = config.services.waffles.lol.port;
+                            ORIGIN = config.services.waffles.lol.hostName;
+                            SHUTDOWN_TIMEOUT = config.services.waffles.lol.shutdownTimeout;
+                            IDLE_TIMEOUT = config.services.waffles.lol.idleTimeout;
+                        };
+                    };
+
+                    systemd.sockets."waffles.lol" = {
+                        listenStreams = [ "${toString config.services.waffles.lol.port}" ];
+                        wantedBy = [ "sockets.target" ];
+                    };
+
+                };
+
+            };
+
         }
     );
 
